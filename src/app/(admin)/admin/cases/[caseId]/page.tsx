e039dbc99e543a5f039110bb36fb0a5ed8f7da5b
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2026-05-04 08:27:40 +0200

    fix

---
+++ src/app/(admin)/admin/cases/[caseId]/page.tsx
@@ -0,0 +1,7 @@
+export default function NewCasePage() {
+	return (
+		<div>
+			<h1>Nuevo Caso</h1>
+		</div>
+	);
+}
---
+++ src/app/(admin)/admin/cases/new/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminCasesPage() {
+	return (
+		<div>
+			<h1>Casos</h1>
+		</div>
+	);
+}
